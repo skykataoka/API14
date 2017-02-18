c29623d18eeb676657863a125c15c0eaeb88381c
KataokaSusumu <skykataoka@gmail.com>
2017-02-18 17:00:11 +0900

    課題提出

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,6 +1,11 @@
 class PoemsController < ApplicationController
   def index
-    @poem = Poem.all
+    @poems = Poem.all
+    render json: @poems
+  end
+
+  def show
+    @poem = Poem.find(params[:id])
     render json: @poem
   end
 end
